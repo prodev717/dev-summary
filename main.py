5311b7feeec00f0befef4023a07fb7e276967b50
Ganesh M <blackboard717@gmail.com>
2024-12-27 22:10:30 +0530

    base flask and htmx code

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/test")
+def test():
+    return "<h1>HTMX test</h1>"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=8000)
+
 

72b79fbb0b0de80ed562613a48ca2544f57afe6b
Ganesh M <blackboard717@gmail.com>
2024-12-28 16:13:37 +0530

    completed function to fetch data from github api

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import requests
+import base64
+
+def getReadmes(username):
+    response = requests.get(f"https://api.github.com/users/{username}/repos").json()
+    repos = {x["name"]:{"description":x["description"],"language":x["language"],"topics":x["topics"],"readme":None} for x in response}
+    for repo in repos:
+        repos[repo]["readme"] = base64.b64decode(requests.get(f"https://api.github.com/repos/{username}/{repo}/contents/README.md").json()["content"]).decode("utf-8")
+    return repos
 
 app = Flask(__name__)
 
@@ -6,11 +15,12 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/test")
-def test():
-    return "<h1>HTMX test</h1>"
+@app.route("/inp",methods=["POST"])
+def inp():
+    username = request.form.get("username")
+    return getReadmes(username)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=8000)
+    app.run(debug=True,host="0.0.0.0",port=8000)
 
 

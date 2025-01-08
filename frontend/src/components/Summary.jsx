3ba52af203fd9c007a090e1a92fb2231195d57fa
muhammadsajid04 <muhammadsajidy04@gmail.com>
2025-01-08 23:09:31 +0530

    Connected with devinfo backend

--- frontend/src/components/Summary.jsx
+++ frontend/src/components/Summary.jsx
@@ -5,12 +5,11 @@ export default function DevSummary() {
     const [userSummary, setUserSummary] = useState(null); 
 
     const handleSummary = () => {
-        fetch('http://localhost:8000/fetch', {
-            method: 'POST',
+        fetch(`http://localhost:8000/devinfo/${username}`, {
+            method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ username: username }),
+            }
         })
         .then(res => res.json())
         .then(data => {
@@ -23,8 +22,8 @@ export default function DevSummary() {
     };
 
     return (
-        <div className="sm:w-[100%] lg:w-[70%] h-screen flex flex-col items-center py-8 font-poppins">
-            <div className="sm:w-[85%] h-16 lg:w-[80%] bg-[#5C5470] rounded-lg border-[#FAF0E6] border-4 flex flex-col justify-center items-center motion-preset-fade motion-duration-[2.5s]">
+        <div className="min-h-screen w-screen max-w-full max-h-full bg-[#352F44] bg-fixed flex flex-col items-center">
+            <div className="sm:w-[85%] h-16 lg:w-[60%] bg-[#5C5470] rounded-lg border-[#FAF0E6] border-4 flex flex-col justify-center items-center motion-preset-fade motion-duration-[2.5s]">
                 <div className="w-[100%] px-2 flex items-center">
                     <input
                         type="text"
@@ -40,14 +39,14 @@ export default function DevSummary() {
                     </button>
                 </div>
             </div>
-            <div className="sm:w-[85%] lg:w-[80%] h-fit bg-[#5C5470] rounded-lg border-[#FAF0E6] border-4 mt-10 flex flex-col justify-center items-center px-2 py-4">
+            <div className="sm:w-[85%] lg:w-[60%] h-fit bg-[#5C5470] rounded-lg border-[#FAF0E6] border-4 mt-10 flex flex-col justify-center items-center px-2 py-4">
                 {userSummary ? (
                     <div className="text-[#FAF0E6] border-[#FAF0E6]">
-                        <h1 className='text-2xl'><strong>{userSummary.summary}</strong></h1>
-                        <p className='my-2'>{userSummary.details}</p>
+                        <h1 className='text-2xl text-center'><strong>Summary of {username}</strong></h1>
+                        <p className='my-2'>{userSummary.summary}</p>
                     </div>
                 ) : (
-                    <p className="text-[#FAF0E6]">
+                    <p className="text-[#FAF0E6] text-md">
                         The summary of the user's GitHub profile will be displayed here
                     </p>
                 )}
